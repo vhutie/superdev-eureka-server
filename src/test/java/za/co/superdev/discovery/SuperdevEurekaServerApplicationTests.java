81875b73392c3b79efa48dcc9dbb9233d8b910a5
Vutlhari Ndlovhu <vhutie@gmail.com>
2020-11-13 13:25:21 +0200

    Initial Push

---
+++ src/test/java/za/co/superdev/discovery/SuperdevEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.superdev.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperdevEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
